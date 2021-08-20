060109a77a9fefe16a46a40c0ceac9a750b04675
Ktgami <89045875+Ktgami@users.noreply.github.com>
2021-08-21 01:13:11 +0500

    Add files via upload

---
+++ CommonDivision/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(getGreatestCommonDivision(9,30));
+
+    }
+    public static int getGreatestCommonDivision(int first,int second){
+            int min =first;
+            int commonDivision =0;
+            if(first < 10 || second < 10){
+                return -1;
+
+            }
+            if (first > second) {
+                min=second;
+            }
+
+        for (int i = min; i > 0; i--) {
+
+            if((first % i==0) &&(second % i==0)){
+                commonDivision = i;
+                break;
+            }
+
+        }
+        return commonDivision;
+
+
+
+    }
+
+
+
+}
