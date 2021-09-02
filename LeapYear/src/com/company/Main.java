3db592b109e6f235a3003d4993baaa9cb21f15c0
Ktgami <89045875+Ktgami@users.noreply.github.com>
2021-09-02 16:28:04 +0500

    Add files via upload

---
+++ LeapYear/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LeapYear zYearNow = new LeapYear();
+
+
+
+
+
+        System.out.println(zYearNow.isYearLeap(2400));
+
+
+    }
+}
